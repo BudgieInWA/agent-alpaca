e7bcc580aef76b045d28247d807a15a7e660400a
BudgieInWA <benjaminaritter@gmail.com>
2016-05-12 11:00:50 +0800

    Refactor Game template

--- imports/game/templates.js
+++ imports/game/templates.js
@@ -12,7 +12,6 @@ import './templates.html';
 Template.gameScreen.onCreated(function () {
     this.id = new ReactiveVar(null);
     this.doc = new ReactiveVar(null);
-    this.team = new ReactiveVar(null);
     this.autorun(() => {
         this.id.set(FlowRouter.getParam('id'));
         this.subscribe('game.details.spymaster', this.id.get()); // This will provide the full
@@ -23,28 +22,38 @@ Template.gameScreen.onCreated(function () {
         this.doc.set(Games.findOne(this.id.get()));
         console.log(this.doc.get());
     });
-    this.autorun(() => {
-        const game = Games.findOne(this.id.get(), { fields: { players: 1 } });
-        if (game && game.players) {
-            if (_.includes(game.players.red,  Meteor.userId())) this.team.set('red');
-            if (_.includes(game.players.blue, Meteor.userId())) this.team.set('blue');
-        }
-    });
 });
 
 Template.gameScreen.helpers({
     doc() {
         return Template.instance().doc.get();
     },
+});
 
+
+Template.gameDetails.onCreated(function() {
+    this.team = new ReactiveVar(null);
+    this.autorun(() => {
+        const game = Template.currentData(); // For reactivity.
+        if (game && game.players) {
+            _.each(['red', 'blue'], t => {
+                if (_.includes(game.players[t], Meteor.userId())) {
+                    this.team.set(t);
+                }
+            });
+        }
+    });
+});
+
+Template.gameDetails.helpers({
     isSpymaster(userId) {
-        const game = Template.instance().doc.get();
+        const game = this;
         return game.round && game.round.spymasters &&
             (game.round.spymasters.red === userId || game.round.spymasters.blue === userId);
     },
 
     myTurn() {
-        const game = Template.instance().doc.get();
+        const game = this;
         const team = Template.instance().team.get();
         return game && game.round && game.round.turn &&
                 game.round.turn.team === team;
@@ -52,12 +61,12 @@ Template.gameScreen.helpers({
     },
 
     canStartRound() {
-        const game = Template.instance().doc.get();
+        const game = this;
         return !game.round || game.round.isEnded;
     },
 
     canNextTurn() {
-        const game = Template.instance().doc.get();
+        const game = this;
         const team = Template.instance().team.get();
         if (!game.round || game.round.isEnded) return false;
         if (!game.round.turn) return true;
@@ -66,7 +75,7 @@ Template.gameScreen.helpers({
     },
 
     canGiveClue() {
-        const game = Template.instance().doc.get();
+        const game = this;
         const team = Template.instance().team.get();
         if (!game.round || game.round.isEnded) return false;
         if (!game.round.turn) return false;
@@ -75,7 +84,7 @@ Template.gameScreen.helpers({
     },
 
     canGuess() {
-        const game = Template.instance().doc.get();
+        const game = this;
         const team = Template.instance().team.get();
         if (!game.round || game.round.isEnded) return false;
         if (!game.round.turn || game.round.turn.team !== team) return false;
@@ -85,32 +94,33 @@ Template.gameScreen.helpers({
     },
 });
 
-Template.gameScreen.events({
+Template.gameDetails.events({
     'click .start-round, click .next-round'(event, template) {
-        const id = template.id.get();
+        const id = template.data._id;
         methods.startRound.call({ id }, messages.methodCallback("Start Round"));
     },
 
     'click .start-turn, click .next-turn'(event, template) {
-        const id = template.id.get();
+        const id = template.data._id;
         methods.nextTurn.call({ id }, messages.methodCallback("Next Turn"));
     },
 
     'submit .clue-form'(event, template) {
         event.preventDefault();
         const form = event.currentTarget;
-        const id = template.id.get();
+        const id = template.data._id;
         const vals = $(form).serializeArray();
         const clue = _.fromPairs(_.map(vals, o => [o.name, o.value])); // Names are unique.
         methods.giveClue.call({ id, clue }, messages.methodCallback("Give Clue"));
     },
 
     'click .guess .card'(event, template) {
-        const id = template.id.get();
+        const id = template.data._id;
         const cardIndex = event.currentTarget.dataset.index;
         methods.makeGuess.call({ id, cardIndex }, messages.methodCallback("Make Guess"));
     },
 });
+
 
 Template.gameReference.onCreated(function () {
     this.autorun(() => {
